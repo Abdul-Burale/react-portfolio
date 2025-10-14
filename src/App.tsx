57ab12f4be5f5f459fc1bf54aac9de3959e81c1e
Abdirahim  Burale <abdirahimburale@Abdirahims-MacBook-Air.local>
2025-10-14 12:26:57 +0100

    chore: initial portfolio site setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="full-background">
+      <div className="relative flex items-center justify-center overflow-y-auto max-h-screen h-screen font-bold pl-15 -mt-22">
+      <h1 className="text-6xl">Abdihakim Burale</h1>
+      </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
